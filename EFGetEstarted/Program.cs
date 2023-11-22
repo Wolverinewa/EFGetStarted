e4dd440903d94a822751bab86e5c4400591d565b
Willian <wolverinewa@gmail.com>
2023-11-22 16:12:56 -0300

    Carga inicial

---
+++ EFGetEstarted/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFGetEstarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
